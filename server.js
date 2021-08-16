0adfb98c9b5f3dd1db0145bee6ae3d2dfac65f3f
xodms2 <84492791+xodms2@users.noreply.github.com>
2021-08-16 00:15:13 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const faker = require("faker");
+const app = express();
+const port = 8000;
+
+const createUser = () => {
+  const newUser = {
+    _id: faker.datatype.uuid(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    phoneNumber: faker.phone.phoneNumber(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  };
+  return newUser;
+};
+
+const createCompany = () => {
+  const newCompany = {
+    _id: faker.datatype.uuid(),
+    companyName: faker.company.companyName(),
+    address: {
+      street: faker.address.streetAddress(),
+      city: faker.address.city(),
+      state: faker.address.state(),
+      zipCode: faker.address.zipCode(),
+      country: faker.address.country() 
+    }
+  }
+  return newCompany;
+}
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello World" })
+});
+
+app.get("/api/random-url", (req, res) => {
+  res.json({ message : "You have hit the random url congrats!" });
+})
+
+app.get("/api/user/new", (req, res) => {
+  const user = createUser();
+  console.log(user);
+  res.json(user);
+})
+
+app.get("/api/company/new", (req, res) => {
+  const company = createCompany();
+  console.log(company);
+  res.json(company);
+})
+
+app.get("/api/user/company", (req, res) => {
+  const user = createUser();
+  const company = createCompany();
+  console.log(user);
+  console.log(company);
+  res.json({
+    user: user,
+    company: company
+  });
+  
+})
+
+app.listen(port, () => {
+  console.log("App is listening on port ", port);
+});
